149794fe02cc1b23bbd51f34d7f9ef20eb91c325
ngandung <gandungn@gmail.com>
2025-04-15 15:46:24 +0700

    create fake api and layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader"
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  //loading, error,ready, active, finished
+  status: 'loading',
+};
+
+function reduce(state, action) {
+  switch(action.type) {
+    case 'dataReceived':
+      return {...state, questions: action.payload, status: 'ready'};
+    case 'dataFailed':
+      return {...state, status: 'error'};
+    case 'start':
+      return {...state, status: 'active'};
+    default:
+      throw new Error('action is unknown');
+  }
+};
 
 export default function App() {
+  const [{questions, status}, dispatch] = useReducer(reduce, initialState);
+  const totalQuestion = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type: 'dataReceived', payload: data}))
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  }, [dispatch]);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen totalQuestion={totalQuestion} dispatch={dispatch}/>}
+        {status === 'active' && <Question />}
+      </Main>
     </div>
   );
 }
---
+++ src/StartScreen.js
@@ -0,0 +1,9 @@
+import { type } from "@testing-library/user-event/dist/type";
+
+export default function StartScreen({totalQuestion = 0, dispatch}) {
+    return <div className="start">
+        <h2>Welcome to The React Quiz</h2>
+        <h3>{totalQuestion} question to test your react mastery</h3>
+        <button className="btn" onClick={() => dispatch({type: 'start'})}>Let's start</button>
+    </div>
+}
